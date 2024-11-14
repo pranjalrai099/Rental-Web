1e778a89205a0a7f61ab611120c825eec50f81c5
pranjalrai099 <pranjalkrai2004@gmail.com>
2024-11-15 03:08:03 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import MyCart from './MyCart';
 import CartPlace from './CartPlace';
-axios.defaults.baseURL='http://localhost:4000';
+axios.defaults.baseURL='https://rental-web-up14-czlld209p-pranjal-rais-projects.vercel.app/?vercelToolbarCode=h0qr67kCkNzWTiu';
 axios.defaults.withCredentials=true;
 function App() {
 
