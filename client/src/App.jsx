70e54369dde0d9696ccd6ea09a6a827af861b5f0
pranjalrai099 <pranjalkrai2004@gmail.com>
2024-11-15 11:44:12 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import MyCart from './MyCart';
 import CartPlace from './CartPlace';
-axios.defaults.baseURL='https://rental-web-1y-fa4kuh9h9-pranjal-rais-projects.vercel.app';
+axios.defaults.baseURL='https://rental-web-1-backend.onrender.com';
 axios.defaults.withCredentials=true;
 function App() {
 
