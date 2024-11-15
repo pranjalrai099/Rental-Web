d883f5e52550ab640a0d928f3a0b9ffa044bc23a
pranjalrai099 <pranjalkrai2004@gmail.com>
2024-11-15 13:46:28 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -28,6 +28,7 @@ app.use(cors({
  credentials: true,
     origin: "https://rental-web-frontend.onrender.com",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+ allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 function ConnectToDb(){
